731b00684b3b0131ff581172f61ed3fd46222ee1
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2015-04-07 21:43:37 +0200

    Create simple chat

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,5 @@
+Messages = new Meteor.Collection('messages');
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -12,6 +14,25 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+  Template.chat.helpers({
+    messages:function(){
+      return Messages.find({},{sort:{date: -1}});
+    }
+  });
+
+  Template.chat.events({
+    'submit form' : function(event, template){
+      event.preventDefault();
+
+      var messageToInsert = event.target.newMessage.value;
+
+      Messages.insert({text: messageToInsert, date: new Date()});
+
+      event.target.newMessage.value = "";
+      console.log('Message:', messageToInsert);
     }
   });
 }
